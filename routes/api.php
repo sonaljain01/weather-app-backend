6ecf4f61ca78d22ef3bd6d01da271254abac6363
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-08-24 17:16:11 +0530

    add controller to fetch future  weather data

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,15 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WeatherController;
 use App\Http\Controllers\previousWeather;
+use App\Http\Controllers\forecastWeather;
 
 
 Route::post("/location", [WeatherController::class, 'sendDataBasedOnLocation']);
 Route::post("/history", [previousWeather::class, 'fetchDataBasedOnLocation']);
+Route::post("/forecast", [forecastWeather::class, 'fetchDataBasedOnLocation']);
+
+
+
+
 
 
