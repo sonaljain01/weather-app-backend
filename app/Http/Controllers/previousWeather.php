877ddd6ffccff887c2349c594416e2947d54cf7a
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-08-24 17:15:16 +0530

    add controller to fetch previous weather data

---
+++ app/Http/Controllers/previousWeather.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Validator;
+use Http;
+
+class previousWeather extends Controller
+{
+    public function fetchDataBasedOnLocation(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'city' => 'required',
+            "state" => 'required',
+            "country" => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
+        }
+
+        $data = $this->getCoordinatefromCity($request->city, $request->state, $request->country);
+
+        $res = $this->getWeather($data['lat'], $data['long']);
+
+
+        return response()->json($res);
+    }
+    public function getCoordinatefromCity(string $city, string $state, string $country)
+    {
+
+        $ninja_api_url = config("api.ninja_api_url");
+        $ninja_api_key = config("api.ninja_api_key");
+
+        $lat = "";
+        $long = "";
+
+        try {
+            $response = Http::withHeader(
+                'X-Api-Key',
+                $ninja_api_key
+            )->get("$ninja_api_url?city=$city&country=$country");
+
+            foreach ($response->json() as $res) {
+                $validdata = $state;
+                if ($res['state'] == $validdata) {
+                    $lat = $res['latitude'];
+                    $long = $res['longitude'];
+                    $data = [
+                        "lat" => $lat,
+                        "long" => $long
+                    ];
+                    return $data;
+                }
+            }
+
+        } catch (\Exception $e) {
+            echo "<pre>";
+            print_r($e);
+            echo "</pre>";
+        }
+
+    }
+
+    public function getWeather(string $lat, string $long)
+    {
+        $api_base_url = config("api.api_base_url");
+        $precipitation = config("api.precipitation");
+        $precipitation_probability = config("api.precipitation_probability");
+        $temprature = config("api.temprature");
+
+        try {
+
+            $response = Http::withHeaders([
+                "Content-Type" => "application/json",
+            ])->get("$api_base_url?latitude=$lat&longitude=$long&hourly=$temprature,$precipitation_probability,$precipitation&daily=temperature_2m_max&timezone=Asia%2FKolkata&past_days=7&forecast_days=1");
+
+
+            return $response->json();
+
+        } catch (\Exception $e) {
+            echo "<pre>";
+            print_r($e);
+            echo "</pre>";
+        }
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WeatherController;
-
+use App\Http\Controllers\previousWeather;
 
 
 Route::post("/location", [WeatherController::class, 'sendDataBasedOnLocation']);
+Route::post("/history", [previousWeather::class, 'fetchDataBasedOnLocation']);
 
 
